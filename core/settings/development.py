0ecdc5b297d40b160a9557f3c52a7a9ab4634a6c
umakaran uma <umakaranuma1126@gmail.com>
2026-03-01 00:17:50 +0530

    Update database configuration in .env and development settings to use environment variables

--- core/settings/development.py
+++ core/settings/development.py
@@ -1,6 +1,7 @@
 """
 Development settings for hirenow-core-api.
 """
+import os
 from .base import *
 
 DEBUG = True
@@ -9,11 +10,11 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "HireNow",
-        "USER": "root",
-        "PASSWORD": "umasiva1126@",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+        "NAME": os.environ.get("DB_DATABASE", "HireNow"),
+        "USER": os.environ.get("DB_USER", "root"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "umasiva1126@"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
         "OPTIONS": {
             "charset": "utf8mb4",
         },
